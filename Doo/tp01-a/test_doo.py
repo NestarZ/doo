3dca14dcfb7d94bfa41f44b2a4654cd7473d3113
Paul Ecoffet <ecoffet.paul@gmail.com>
2015-02-06 16:34:26 +0100

    Addition of test files

---
+++ Doo/tp01-a/test_doo.py
@@ -0,0 +1,35 @@
+import unittest
+
+from tp01 import Doo
+from tp01 import J_ATT, J_DEF, BLANCS, NOIRS, ROI, VIDE
+
+
+
+class TestDoo(unittest.TestCase):
+
+    def test_listecoup(self):
+        doo = Doo()
+        board = [VIDE, VIDE, VIDE,
+                 VIDE, VIDE, VIDE,
+                 VIDE, BLANCS, VIDE,
+                 VIDE, VIDE, VIDE]
+
+        t = 10
+        doo.configuration = board, t
+        print(doo.listeCoups(J_DEF))
+
+        board = [VIDE, VIDE, VIDE,
+                 VIDE, NOIRS, VIDE,
+                 VIDE, BLANCS, VIDE,
+                 VIDE, VIDE, VIDE]
+        t = 10
+        doo.configuration = board, t
+        print(doo.listeCoups(J_DEF))
+
+        board = [ VIDE, NOIRS, VIDE,
+                 VIDE, BLANCS, ROI,
+                 BLANCS, NOIRS, VIDE,
+                 VIDE, VIDE, VIDE ]
+        t = 10
+        doo.configuration = board, t
+        print(doo.listeCoups(J_DEF))
