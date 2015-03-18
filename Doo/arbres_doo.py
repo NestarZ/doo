8555612ecd61f1b3c5be9f430f120a9b08f7e54f
Paul Ecoffet <ecoffet.paul@gmail.com>
2015-03-18 20:18:02 +0100

    scoring des evaluations en cours. Dummy est le meilleur

--- Doo/arbres_doo.py
+++ Doo/arbres_doo.py
@@ -12,24 +12,46 @@ sur le jeu de Doo
 from tp01b import manche, replay, Human
 from arbres import Parcours, IA
 from tp01a import *
+import itertools
 
-def main(force=3,code=0):
+
+def play_manche(eval1, eval2, force=3,code=0):
     """
     par defaut : une partie 
     minmax recursif (code 0) niveau 3 (force 3)
     contre First
     """
-    jA = IA(force,code, evaluation)
-    jB = IA(force, code, evaluation2) # joueur par défaut
+    jA = IA(force,code, eval1)
+    jB = IA(force, code, eval2) # joueur par défaut
     #B = Human('B')
     print(jA,"force %d" % jA.niveau)
     score, tour, hist = manche(jA,jB)
     # replay(hist)
-    print(hist)
+    return score
 
 
+def main():
+    evaluations = [evaluation1, evaluation2, dummy]
+    score_att = {evaluation: 0 for evaluation in evaluations}
+    score_def = {evaluation: 0 for evaluation in evaluations}
+    for eval1, eval2 in itertools.permutations(evaluations, 2):  # génère toutes les combinaisons d'évaluations, sans self vs self
+        score = play_manche(eval1, eval2, force=4)
+        if score > 0:
+            score_att[eval1] += score
+        else:
+            score_def[eval2] += 1
+    print('défense: ')
+    print(score_def)
+    print('attaque: ')
+    print(score_att)
 
-def evaluation(self, joueur):
+def dummy(self, joueur):
+    if self.perdant(joueur): return -100
+    if self.gagnant(joueur): return 100
+    return 0
+
+
+def evaluation1(self, joueur):
         """
         evalue numeriquement la situation dans lequel se trouve le joueur
         """
@@ -44,7 +66,7 @@ def evaluation(self, joueur):
         else:
             nb_blancs = self.board.count(BLANCS)
             nb_noirs = 0
-            smallest_distance = float("inf")
+            smallest_distance = 3
             for i, pion in enumerate(self.board):
                 if pion == NOIRS or pion == ROI:
                     nb_noirs += 1
@@ -66,15 +88,15 @@ def evaluation2(self, joueur):
         else:
             nb_blancs = self.board.count(BLANCS)
             nb_noirs = 0
-            smallest_distance = float("inf")
+            smallest_distance = 3
             for i, pion in enumerate(self.board):
                 if pion == NOIRS or pion == ROI:
                     nb_noirs += 1
                     smallest_distance = min(smallest_distance, self._distance_from_doo(i))
             return - (smallest_distance)**2 - nb_blancs - abs(nb_noirs - 1)
-    
+
 
 if __name__ == "__main__" :
     # force: la profondeur, code: l'algorithme
-    main(force=4,code=0)
+    main()
 
